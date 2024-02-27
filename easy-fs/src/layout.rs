c0836119b6ac3aa4f207c03a5385b3a44fc79e94
mag1c1an1 <mag1cian@icloud.com>
2024-02-27 11:00:39 +0800

    update
    Signed-off-by: mag1c1an1 <mag1cian@icloud.com>

--- easy-fs/src/layout.rs
+++ easy-fs/src/layout.rs
@@ -1,11 +1,34 @@
+use core::fmt::Debug;
+use core::fmt::Formatter;
+use core::fmt::Result;
+
+use alloc::sync::Arc;
+
+use crate::block_cache::get_block_cache;
+use crate::block_dev::BlockDevice;
+/// Magic number for sanity check
+const EFS_MAGIC: u32 = 0x3b800001;
+/// The max number of direct inodes
 const INODE_DIRECT_COUNT: usize = 28;
-const INODE_INDIRECT1_COUNT: usize = super::BLOCK_SZ / 4;
+/// The max length of inode name
 const NAME_LENGTH_LIMIT: usize = 27;
+/// The max number of indirect1 inodes
+const INODE_INDIRECT1_COUNT: usize = super::BLOCK_SZ / 4;
+/// The max number of indirect2 inodes
+const INODE_INDIRECT2_COUNT: usize = INODE_INDIRECT1_COUNT * INODE_INDIRECT1_COUNT;
+/// The upper bound of direct inode index
+const DIRECT_BOUND: usize = INODE_DIRECT_COUNT;
+/// The upper bound of indirect1 inode index
+const INDIRECT1_BOUND: usize = DIRECT_BOUND + INODE_INDIRECT1_COUNT;
+/// The upper bound of indirect2 inode indexs
+#[allow(unused)]
+const INDIRECT2_BOUND: usize = INDIRECT1_BOUND + INODE_INDIRECT2_COUNT;
+/// Super block of a filesystem
 
 #[repr(C)]
 pub struct SuperBlock {
     magic: u32,
-    pub totoal_blocks: u32,
+    pub total_blocks: u32,
     pub inode_bitmap_blocks: u32,
     pub inode_area_blocks: u32,
     pub data_bitmap_blocks: u32,
@@ -26,6 +49,10 @@ pub enum DiskInodeType {
     File,
     Directory,
 }
+/// A indirect block
+type IndirectBlock = [u32; super::BLOCK_SZ / 4];
+/// A data block
+type DataBlock = [u8; super::BLOCK_SZ];
 
 #[repr(C)]
 pub struct DirEntry {
@@ -35,6 +62,97 @@ pub struct DirEntry {
 
 pub const DIRENT_SZ: usize = 27 + 1 + 4;
 
+impl Debug for SuperBlock {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        f.debug_struct("SuperBlock")
+            .field("total_blocks", &self.total_blocks)
+            .field("inode_bitmap_blocks", &self.inode_bitmap_blocks)
+            .field("inode_area_blocks", &self.inode_area_blocks)
+            .field("data_bitmap_blocks", &self.data_bitmap_blocks)
+            .field("data_area_blocks", &self.data_area_blocks)
+            .finish()
+    }
+}
 
+impl SuperBlock {
+    /// Initialize a super block
+    pub fn initialize(
+        &mut self,
+        total_blocks: u32,
+        inode_bitmap_blocks: u32,
+        inode_area_blocks: u32,
+        data_bitmap_blocks: u32,
+        data_area_blocks: u32,
+    ) {
+        *self = Self {
+            magic: EFS_MAGIC,
+            total_blocks,
+            inode_bitmap_blocks,
+            inode_area_blocks,
+            data_bitmap_blocks,
+            data_area_blocks,
+        }
+    }
+    /// Check if a super block is valid using efs magic
+    pub fn is_valid(&self) -> bool {
+        self.magic == EFS_MAGIC
+    }
+}
 
+impl DiskInode {
+    /// Return block number correspond to size.
+    pub fn data_blocks(&self) -> u32 {
+        Self::_data_blocks(self.size)
+    }
 
+    fn _data_blocks(size: u32) -> u32 {
+        (size + super::BLOCK_SZ as u32 - 1) / super::BLOCK_SZ as u32
+    }
+
+    pub fn total_blocks(size: u32) -> u32 {
+        let data_blocks = Self::_data_blocks(size) as usize;
+        let mut total = data_blocks;
+        // 1
+        if data_blocks > INODE_DIRECT_COUNT {
+            total += 1;
+        }
+        // 2
+        if data_blocks > INDIRECT1_BOUND {
+            total += 1;
+            // up bound
+            total +=
+                (data_blocks - INDIRECT1_BOUND + INODE_INDIRECT1_COUNT - 1) / INODE_INDIRECT1_COUNT;
+        }
+        total as u32
+    }
+
+    pub fn blocks_num_needed(&self, new_size: u32) -> u32 {
+        assert!(new_size >= self.size);
+        Self::total_blocks(new_size) - Self::total_blocks(self.size)
+    }
+
+    pub fn get_block_id(&self, inner_id: u32, block_device: &Arc<dyn BlockDevice>) -> u32 {
+        let inner_id = inner_id as usize;
+        if inner_id < INODE_DIRECT_COUNT {
+            self.direct[inner_id]
+        } else if inner_id < INDIRECT1_BOUND {
+            get_block_cache(self.indirect1 as usize, Arc::clone(block_device))
+                .lock()
+                .read(0, |indirect_block: &IndirectBlock| {
+                    indirect_block[inner_id - INODE_DIRECT_COUNT]
+                })
+        } else {
+            let last = inner_id - INDIRECT1_BOUND;
+            let indirect1 = get_block_cache(self.indirect2 as usize, Arc::clone(block_device))
+                .lock()
+                .read(0, |indirect2: &IndirectBlock| {
+                    indirect2[last / INODE_INDIRECT1_COUNT]
+                });
+            get_block_cache(indirect1 as usize, Arc::clone(block_device))
+                .lock()
+                .read(0, |indirect1: &IndirectBlock| {
+                    indirect1[last % INODE_INDIRECT1_COUNT]
+                })
+        }
+    }
+}
